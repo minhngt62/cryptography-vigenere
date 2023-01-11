594aab3282a82d13cea93fc613df12d7fd3ccd92
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-01-11 16:42:40 +0700

    Coding skeleton

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+DEMO VIGENERE CIPHER
+    1. plaintext, key --> VigenereCipher() --> ciphertext
+    2. ciphertext --> Kasiski method --> key length
+    3. ciphertext, key length --> cryptanalysis --> plaintext
+'''
---
+++ vigenere_cipher/attack/examination.py
@@ -0,0 +1,1 @@
+# KASISKI METHOD
