992b29338ff6cfb7ebcd8741dbbdeb6d0ed76aab
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2023-01-31 22:45:11 +0700

    add generate_basekey function in keygen.py

--- vigenere_cipher/cryption/keygen.py
+++ vigenere_cipher/cryption/keygen.py
@@ -1,7 +1,13 @@
 import random
 import string
 
-def generate_key(
+def generate_basekey(
     key_length: int
     ) -> str:
     return ''.join(random.choice(string.ascii_letters[26:]) for x in range(key_length))
+
+def generate_key(length):
+    baseKey = generate_basekey(length)
+    baseKeyLength = len(baseKey)
+    key = [baseKey[i % baseKeyLength] for i in range(0, length)]
+    return key
