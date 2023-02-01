4486751227d73cfe78c359d4f224a1ed28395d77
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2023-02-01 23:05:22 +0700

    update cryptanalysis.py

--- vigenere_cipher/attacking/cryptanalysis.py
+++ vigenere_cipher/attacking/cryptanalysis.py
@@ -1,7 +1,7 @@
-import sys
-sys.path.append('/Users/lggvu/Projects/HUST/Crypto/cryptography-vigenere/vigenere_cipher/attacking')
+import examination
 import processing
 import frequency_analysis as fa
+from const import *
 import string
 
 
@@ -13,3 +13,29 @@ def _decypher(cyphertext, key):
     decyphered_blocks = processing.to_blocks([fa.shift(col, shift) for col, shift in zip(cols, shifts)])
     decyphered = ''.join(decyphered_blocks)
     return decyphered
+
+
+def attack(file):
+    with open(file) as f:
+        cyphertext = f.readlines()
+        key_len = 0
+        key_len = examination.estimateKeyLength(cyphertext[0])
+
+        key = fa.restore_key(cyphertext[0], key_len)
+        decyphered = _decypher(cyphertext[0], key)
+        print('Chosen key length: '+str(key_len))
+        print('Restored key: '+str(key))
+        print('Plaintext: '+str(decyphered))
+
+
+
+if __name__ == '__main__':
+    file = '/Users/lggvu/Projects/HUST/Crypto/creamcrackerz/encrypted/test-2.txt'
+    with open(file) as f:
+        cyphertext = f.readlines()
+    print("Cyphertext: ", cyphertext[0])
+    print('Applying kasiski examination\n')
+
+
+    attack(file)
+
