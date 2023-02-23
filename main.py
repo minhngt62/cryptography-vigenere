065de4a6207c380074f3134617629b7a75fa88f3
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-02-23 12:38:35 +0700

    Correct test case and complete the demo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from vigenere_cipher.attacking import attack
+from vigenere_cipher.cryption import VigenereCipher
 
 from functools import reduce
 
@@ -10,12 +11,29 @@ DEMO VIGENERE CIPHER
 '''
 
 def main():
+    # load the test data
     file = 'test.txt'
+    print(">> [SYSTEM] Loading test text")
     with open(file) as f:
-        cyphertext = f.readlines()
-    print("Cyphertext: ", cyphertext[0])
+        text = f.readlines()
+    print("Plaintext:", text[1])
+    print("Key:", text[2])
+    print("Ciphertext:", text[0])
+
+    # vigenere cipher
+    print('>> [SYSTEM] Applying Vigenere Cipher......')
+    cypher = VigenereCipher(key=text[2].strip())
     
-    print('---APPLYING KASISKI EXAMINATION---')
+    ciphertext = cypher.encrypt(text[1].strip())
+    assert ciphertext == text[0].strip()
+    print(">> [ENCODER] Encryption assertions passed")
+
+    decrypt_text = cypher.decrypt(ciphertext)
+    assert decrypt_text == text[1].strip()
+    print(">> [DECODER] Decryption assertions passed")
+    
+    # kasiski exam
+    print('\n>> [SYSTEM] Start attacking with Kasiski examination......')
     attack(file)
 
 
